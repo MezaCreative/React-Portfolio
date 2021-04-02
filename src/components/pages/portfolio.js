4e10f81292ca3aedcc0334e8776750a1ebe16a37
Esteban Meza <meza.19@osu.edu>
2021-04-02 01:48:38 -0400

    added photos of past works

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -1,5 +1,10 @@
  import React from 'react';
-
+ import noteFoto from './assets/Note taker.png';
+ import tempFoto from './assets/template.png';
+ import weatherFoto from './assets/weather.png';
+ import inRhythmFoto from './assets/landing.png';
+ import workoutFoto from './assets/workout.png';
+ import covidFoto from './assets/image.png';
  function Portfolio() {
       return (
         <div className="container">
@@ -14,7 +19,7 @@
         <div className="card-container">
           <div className="row cards">
             <div className="card col-4" style={{width: '18rem'}}>
-              <img src="assets/Note taker.png" className="card-img-top" alt="Note-taker app" />
+              <img src={noteFoto} className="card-img-top" alt="Note-taker app" />
               <div className="card-body">
                 <h5 className="card-title">Note Taker</h5>
                 <p className="card-text">App that can write, save, and delete notes. Uses an express backend to save and retrieves note data from a JSON file</p>
@@ -22,7 +27,7 @@
               </div>
             </div>
             <div className="card col-4" style={{width: '18rem'}}>
-              <img src="assets/template.png" className="card-img-top" alt="Template Enging app" />
+              <img src={tempFoto} className="card-img-top" alt="Template Enging app" />
               <div className="card-body">
                 <h5 className="card-title">Template Engine</h5>
                 <p className="card-text">A software engingeering team generator command line application.</p>
@@ -30,9 +35,9 @@
               </div>
             </div>
             <div className="card col-4" style={{width: '18rem'}}>
-              <img src="assets/weather.png" className="card-img-top" alt="Weather Dashboard" />
+              <img src={weatherFoto} className="card-img-top" alt="Weather Dashboard" />
               <div className="card-body">
-                <h5 className="card-title">Template Engine</h5>
+                <h5 className="card-title">Weather Dashboard</h5>
                 <p className="card-text">Weather dashboard app that runs in browser and features dynamically updated HTML, CSS, and OpenWeather API.</p>
                 <a href="https://mezacreative.github.io/weather-dashboard/" className="btn btn-primary">Link</a>
               </div>
@@ -40,7 +45,7 @@
           </div>
           <div className="row cards">
             <div className="card col-4" style={{width: '18rem'}}>
-              <img src="assets/inrythmlanding.png" className="card-img-top" alt="inRhythmapp" />
+              <img src={inRhythmFoto} className="card-img-top" alt="inRhythmapp" />
               <div className="card-body">
                 <h5 className="card-title">inRhythm</h5>
                 <p className="card-text">App that predicts a users Biorhythm with login functioncality to save data.</p>
@@ -48,7 +53,7 @@
               </div>
             </div>
             <div className="card col-4" style={{width: '18rem'}}>
-              <img src="assets/workoutlanding.png" className="card-img-top" alt="Workout Tracker app" />
+              <img src={workoutFoto} className="card-img-top" alt="Workout Tracker app" />
               <div className="card-body">
                 <h5 className="card-title">Workout Tracker</h5>
                 <p className="card-text">App that tracks your workouts and saves info in database.</p>
@@ -56,7 +61,7 @@
               </div>
             </div>
             <div className="card col-4" style={{width: '18rem'}}>
-              <img src="assets/covidLanding.png" className="card-img-top" alt="Weather Dashboard" />
+              <img src={covidFoto} className="card-img-top" alt="Weather Dashboard" />
               <div className="card-body">
                 <h5 className="card-title">Covid-19 Tracker</h5>
                 <p className="card-text">App that give up-to-date stats of Covid-19 cases in the U.S</p>
