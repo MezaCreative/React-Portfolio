64b9c957e1ee51bb2f80291aefc205b1bd98489c
Esteban Meza <meza.19@osu.edu>
2021-04-02 00:32:14 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './assets/Meza-logo.png';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import Index from './components/pages/index';
-
+import Index from './components/pages/Index';
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Index/>
+      <div>
+        <Switch>
+          <Route exact path={["/", "/index"]}>
+              <Index/>
+          </Route>
+          <Route exact path={["/portfolio", "/portfolio"]}>
+                <Portfolio/>
+          </Route >
+
+          <Route exact path={["/contact", "/contact"]}>
+                <Contact/>
+          </Route>
+
+        </Switch>
+            
+      </div>
+
+
     <Footer/>
     </div>
   );
